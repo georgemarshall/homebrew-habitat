11e5c2028ae39079574c3955ff784eac79be9349
George Marshall <george@georgemarshall.name>
2017-07-17 01:46:00 -0700

    Initial import
    Signed-off-by: George Marshall <george@georgemarshall.name>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Habitat for Homebrew
+
+Want to try Habitat? [Get started here](https://www.habitat.sh/try/).
---
+++ habitat.rb
@@ -0,0 +1,32 @@
+#
+# Copyright:: Copyright (c) 2016-2017 Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+class Habitat < Formula
+  desc "Automation That Travels with the App"
+  homepage "https://www.habitat.sh/"
+  url "https://dl.bintray.com/habitat/stable/darwin/x86_64/hab-0.26.1-20170715032253-x86_64-darwin.zip"
+  version "0.26.1-20170715032253"
+  sha256 "5835891dbb8f7d5a14c153d182cbfa17cff0c835c0ac90cb7019f893ff694b16"
+
+  def install
+    bin.install "hab"
+  end
+
+  test do
+    assert_match version.to_s.gsub('-', '/'), shell_output("#{bin}/hab --version 2>&1")
+  end
+end
